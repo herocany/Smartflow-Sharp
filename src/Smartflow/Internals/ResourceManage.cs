137099dc99d18c1d7756f88134fc4d88e2420fe1
chengderen <38171858+chengderen@users.noreply.github.com>
2019-12-22 11:16:22 +0800

    去掉资源配置文件

--- src/Smartflow/Internals/ResourceManage.cs
+++ src/Smartflow/Internals/ResourceManage.cs
@@ -17,6 +17,20 @@ namespace Smartflow.Internals
     /// </summary>
     internal class ResourceManage
     {
+
+        private static readonly Dictionary<string, string> resourceConfig = new Dictionary<string, string>();
+
+        static ResourceManage()
+        {
+            resourceConfig.Add("MAIL_URL_EXPRESSION", @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$");
+            resourceConfig.Add("SQL_ACTOR_RECORD", "SELECT Y.ID, X.Name, X.CreateDateTime FROM T_ACTOR X, T_NODE Y WHERE X.InstanceID = @InstanceID AND X.RelationshipID = Y.NID ORDER BY CREATEDATETIME ASC");
+            resourceConfig.Add("SQL_WORKFLOW_INSTANCE", "SELECT X.InstanceID, X.RelationshipID, X.Resource, X.State, X.Mode, Y.Name, Y.NID, Y.ID, Y.NodeType, Y.InstanceID, Y.Cooperation FROM T_INSTANCE X INNER JOIN  T_NODE Y  ON(X.InstanceID = Y.InstanceID AND X.RelationshipID = Y.ID) WHERE X.InstanceID = @InstanceID");
+            resourceConfig.Add("SQL_WORKFLOW_PROCESS_LATEST", "SELECT * FROM T_PROCESS WHERE InstanceID=@InstanceID   AND Command=@Command ORDER BY CreateDateTime DESC");
+            resourceConfig.Add("SQL_WORKFLOW_PROCESS_RECORDS", "SELECT Origin,Destination,(SELECT ID FROM T_TRANSITION T WHERE T.NID=X.TransitionID) ID FROM T_PROCESS X WHERE InstanceID=@InstanceID AND Command=0 ORDER BY CreateDateTime ASC");
+            resourceConfig.Add("SQL_WORKFLOW_TRANSITION_RECORD", "SELECT [NID],[RELATIONSHIPID],[NAME],[DESTINATION],[ORIGIN],[INSTANCEID],[EXPRESSION],[ID] FROM T_TRANSITION WHERE NID=@NID");
+        }
+
+
         /// <summary>
         /// 参与过审批操作信息
         /// </summary>
@@ -42,21 +56,14 @@ namespace Smartflow.Internals
         /// </summary>
         public const string SQL_WORKFLOW_TRANSITION_RECORD = "SQL_WORKFLOW_TRANSITION_RECORD";
 
-
         /// <summary>
         /// 验证MAIL地址，正则表达式
         /// </summary>
         public const string MAIL_URL_EXPRESSION = "MAIL_URL_EXPRESSION";
 
-        public const string SMARTFLOW_SHARP_NAME = "SMARTFLOW_SHARP_NAME";
-        public const string SMARTFLOW_SHARP_TITLE = "SMARTFLOW_SHARP_TITLE";
-        public const string SMARTFLOW_SHARP_VERSION = "SMARTFLOW_SHARP_VERSION";
-
-        private static readonly ResourceManager resourceManage =new ResourceManager("Smartflow.Internals.SmartflowResource", Assembly.GetExecutingAssembly());
-        
         public static string GetString(string key)
         {
-            return resourceManage.GetString(key);
+            return resourceConfig[key];
         }
     }
 }
